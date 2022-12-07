f28f08aefa28d6e6cf17ab26c64b29fe3f60d7ab
Sheelove Dol <siesel26@gmail.com>
2022-12-06 16:13:59 -0800

    Created both the movies and the tvSeries pages

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,8 +1,23 @@
-
+import Card from "../components/Card"
+import CardList from "../components/CardList";
+import { useGlobalContext } from "../context";
 
 const Movies = () => {
+
+  const { data } = useGlobalContext();
+  const movies = data.filter((item) => item.category === "Movie")
+  
   return (
-    <div>This is the Movies page</div>
+    <div className=" grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 justify-center">
+      {
+        movies.map((item, i) => (
+          <Card 
+            key={i}
+            item={item}
+          />
+        ))
+      }
+    </div>
   )
 }
 
--- src/pages/TVseries.jsx
+++ src/pages/TVseries.jsx
@@ -1,8 +1,24 @@
+import Card from "../components/Card"
+import { useGlobalContext } from "../context"
+
 
 
 const TVseries = () => {
+
+  const { data } = useGlobalContext()
+  const tvSeries = data.filter((item) => item.category === "TV Series");
+
   return (
-    <div>TVseries</div>
+    <div className=" grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 justify-center">
+      {
+        tvSeries.map((item, i) => (
+          <Card 
+            key={i}
+            item={item}
+          />
+        ))
+      }
+    </div>
   )
 }
 
