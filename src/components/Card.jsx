5c63b8b49a56896ff32a3da80e1790050e15b375
Sheelove Dol <siesel26@gmail.com>
2022-12-11 01:08:51 -0800

    Created PlayButton component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import movieIcon from "../assets/icon-category-movie.svg";
 import tvSeriesIcon from "../assets/icon-category-tv.svg";
-import playButton from "../assets/icon-play.svg"
+import PlayButton from "./PlayButton"
 
 const Card = ({ item }) => {
 
@@ -17,12 +17,7 @@ const Card = ({ item }) => {
           className={`w-full group bg-cover cursor-pointer h-[7rem] rounded-lg`}
           style={{ backgroundImage: `url('${getImageUrl()}')` }}
         >
-          <div className="w-full h-full rounded-lg bg-slate-800 bg-opacity-50 flex justify-center items-center invisible group-hover:visible">
-            <div className=" flex gap-3 align-middle rounded-full p-1 pr-3 bg-slate-300 bg-opacity-30">
-              <img  className="h-4" src={playButton} />
-              <p className="text-xs flex items-center">Play</p>
-            </div>
-          </div>
+          <PlayButton />
         </div>
       </divb>
       <div className="py-1 text-[0.65rem] text-slate-500 flex items-center gap-2">
---
+++ src/components/PlayButton.jsx
@@ -0,0 +1,14 @@
+import playButton from "../assets/icon-play.svg";
+
+const PlayButton = () => {
+  return (
+    <div className="w-full h-full rounded-lg bg-slate-800 bg-opacity-50 flex justify-center items-center invisible group-hover:visible">
+      <div className=" flex gap-3 align-middle rounded-full p-1 pr-3 bg-slate-300 bg-opacity-30">
+        <img className="h-4" src={playButton} />
+        <p className="text-xs flex items-center">Play</p>
+      </div>
+    </div>
+  );
+};
+
+export default PlayButton;
