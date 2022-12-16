c914363a39e1df53da734f6328a2cd3fad71ec72
Sheelove Dol <siesel26@gmail.com>
2022-12-16 13:14:23 -0800

    Added animation to play button

--- src/components/PlayButton.jsx
+++ src/components/PlayButton.jsx
@@ -3,7 +3,7 @@ import playButton from "/assets/icon-play.svg";
 const PlayButton = () => {
   return (
     <div className="w-full h-full rounded-lg bg-slate-800 bg-opacity-50 flex justify-center items-center invisible group-hover:visible">
-      <div className=" flex gap-3 align-middle rounded-full p-1 pr-3 bg-slate-300 bg-opacity-30">
+      <div className="hover:animate-pulse hover:scale-125 flex gap-3 align-middle rounded-full p-1 pr-3 bg-slate-300 bg-opacity-30">
         <img className="h-4" src={playButton} />
         <p className="text-xs flex items-center">Play</p>
       </div>
